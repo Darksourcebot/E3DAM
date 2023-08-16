8198e9bacff530b78822e339485c0d01376835aa
testingelnqyb <138293704+testingelnqyb@users.noreply.github.com>
2023-08-16 14:38:55 +0300

    jh

--- ElNqYbMusic/__main__.py
+++ ElNqYbMusic/__main__.py
@@ -60,17 +60,6 @@ async def init():
     )
     await userbot.start()
     await ElNqYb.start()
-    try:
-        await ElNqYb.stream_call(
-            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
-        )
-    except NoActiveGroupCall:
-        LOGGER("ElNqYbMusic").error(
-            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
-        )
-        sys.exit()
-    except:
-        pass
     await ElNqYb.decorators()
     LOGGER("ElNqYbMusic").info("ElNqYb Music Bot Started Successfully")
     await idle()
